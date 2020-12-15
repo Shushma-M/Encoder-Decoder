4bb14a313d476470a33f2f494f16c017dfd93b54
Shushma-M <shushmagowda22@gmail.com>
2020-12-15 13:00:42 +0530

    Initial commit
    Encodes and decodes Morse code

---
+++ app.js
@@ -0,0 +1,32 @@
+var url="https://api.funtranslations.com/translate/morse.json";
+//var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var inpTxt=document.querySelector("#input-txt");
+var opTxt=document.querySelector("#output-txt");
+var btnClick=document.querySelector("#btn");
+var output;
+
+function refresh(){
+    location.reload();
+}
+
+function errorHandle(error){
+    console.log("Error occured:",error);
+    alert("Oops!! Something went wrong please try again later");
+}
+
+function eventHandler() {
+    console.log("1button");
+    var ip=inpTxt.value;
+    var urlUpdate=""+url+"?text="+ip;
+    fetch(urlUpdate)
+    .then (r => r.json())
+    .then (json => 
+        {output=json.contents.translated
+            opTxt.innerText=""+output})
+    .catch(errorHandle);
+};
+
+btnClick.addEventListener("click",eventHandler);
+
+
+
---
+++ app2.js
@@ -0,0 +1,21 @@
+var inpTxt2=document.querySelector("#input-txt2");
+var opTxt2=document.querySelector("#output-txt2");
+var btn2Click=document.querySelector("#btn2");
+var output2;
+var url2="https://api.funtranslations.com/translate/morse2english.json";
+function errorHandle(error){
+    console.log("Error occured:",error);
+    alert("Oops!! Something went wrong please try again later");
+}
+function eventHandler2() {
+    console.log("2button");
+    var ip=inpTxt2.value;
+    var urlUpdate=""+url2+"?text="+ip;
+    fetch(urlUpdate)
+    .then (r => r.json())
+    .then (json => 
+        {output2=json.contents.translated
+            opTxt2.innerText=""+output2})
+    .catch(errorHandle);
+};
+btn2Click.addEventListener("click",eventHandler2);
